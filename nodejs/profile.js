bb9e7e85aa7bbdb43e86c4712aa82292bfd9e5e7
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-02-26 20:05:44 -0500

    Add http module for showing error codes.

--- nodejs/profile.js
+++ nodejs/profile.js
@@ -1,6 +1,7 @@
 //Problem: We need a simple way to look at a user's badge count and Javascript points
 //Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 var https = require("https");
+var http = require("http");
 
 //Print out message
 function printMessage(username, badgeCount, points) {

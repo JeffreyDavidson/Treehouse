b3d9fc8f8d30545d394945ba2bb3574b6a7fd410
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-03-02 19:24:52 -0500

    Add random number functionality.

---
+++ javascript/javascript basics/random.js
@@ -0,0 +1,18 @@
+function getRandom(upper) {
+	var randomNumber = Math.floor(Math.random() * upper) + 1;
+	return randomNumber;
+}
+
+/*
+console.log(getRandomNumber(6));
+console.log(getRandomNumber(100));
+console.log(getRandomNumber(10000));
+console.log(getRandomNumber(2));
+*/
+
+function getArea(width, length) {
+	var area = width * length;
+	return area + " " + unit;
+}
+
+console.log(getArea(10, 20, 'sq ft'));

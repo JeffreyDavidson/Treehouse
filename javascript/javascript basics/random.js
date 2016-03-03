3a5c7fd070eca8c3c9ec8bedd9f72e0eb3a7ec95
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-03-02 19:30:33 -0500

    Add additional arugments and function parameters for more random numbers.

--- javascript/javascript basics/random.js
+++ javascript/javascript basics/random.js
@@ -1,14 +1,14 @@
-function getRandom(upper) {
-	var randomNumber = Math.floor(Math.random() * upper) + 1;
-	return randomNumber;
+function getRandom(lower, upper) {
+	var random = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+	return random;
 }
 
-/*
-console.log(getRandomNumber(6));
-console.log(getRandomNumber(100));
-console.log(getRandomNumber(10000));
-console.log(getRandomNumber(2));
-*/
+
+console.log(getRandomNumber(1, 6));
+console.log(getRandomNumber(1, 100));
+console.log(getRandomNumber(200, 500));
+console.log(getRandomNumber(50, 100));
+
 
 function getArea(width, length) {
 	var area = width * length;

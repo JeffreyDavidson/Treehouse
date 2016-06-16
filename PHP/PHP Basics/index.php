3bdf7ee4a09ebd293f5e31c7581ef2d1a424e57b
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-06-16 12:00:37 -0400

    Learned about floating point numbers.
    I was taught what a floating point number type is and how they can be used just like integer values in equations and how they can also be combined with who integer values.

--- PHP/PHP Basics/index.php
+++ PHP/PHP Basics/index.php
@@ -39,11 +39,14 @@ $name = $full_name;
                 $three = 3;
                 $string_one = "1";
 
+                $distance_to_home = 1.2;
+                $distance_to_work = 2.5
 
                 ?>
             </pre>
             <ul>
-                <li><?php echo $one + $two - $three ?></li>
+                <li><?php echo $one + $two - $three; ?></li>
+                <li><?php echo $distance_to_work + $distance_to_home + $three; ?></li>
             </ul>
         </section>
     </body>

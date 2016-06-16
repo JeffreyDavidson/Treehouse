980a35df5923c74a1a116674e5bf7139fe571312
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-06-16 11:44:41 -0400

    Restructuring part 2

---
+++ PHP/PHP Basics/index.php
@@ -0,0 +1,51 @@
+<?php
+// This is my first name.
+$name = "Jeff";
+
+
+$location = "Orlando, FL";
+$full_name = "Mike the Frog";
+$name = $full_name;
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8>
+        <title><?php echo $name; ?> | Treehouse Profile</title>
+        <link href="css/style.css" rel="stylesheet"/>
+    </head>
+
+    <body>
+        <section class="sidebar text-center">
+            <div class="avatar">
+                <img src="img/avatar.png" alt="<?php echo $name; ?>">
+            </div>
+            <h1><?php echo $name; ?></h1>
+            <p><?php  echo $location; ?></p>
+            <hr/>
+            <p>Welcome to PHP Basics!</p>
+            <hr/>
+            <ul class="social">
+                <li><a href=""><span class="icon twitter"></span></a></li>
+            </ul>
+        </section>
+        <section class="main">
+            <pre>
+                <?php
+                $one = 1;
+                $two = 2;
+                $three = 3;
+                $string_one = "1";
+
+
+                ?>
+            </pre>
+            <ul>
+                <li><?php echo gettype($one) ?></li>
+                <li><?php echo gettype($string_one) ?></li>
+            </ul>
+        </section>
+    </body>
+</html>

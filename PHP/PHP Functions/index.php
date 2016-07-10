aac0547c4568cc331611913df5445668586f8829
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-07-09 22:27:47 -0400

    Learned about closures.
    I learned what closures are and that they can also access variables outside of the function but you have to use the "use" keyword for it to have access to the variable.

--- PHP/PHP Functions/index.php
+++ PHP/PHP Functions/index.php
@@ -1,15 +1,9 @@
 <?php
 
-function answer() {
-    return 42;
-}
+$name = 'Jeffrey';
 
-function add_up($a, $b) {
-    return $a + $b;
-}
+$greet = function()  use($name) {
+    echo "Hello $name!";
+};
 
-$func = 'add_up';
-
-$num =  $func(5, 10);
-
-echo $num;
+$greet();

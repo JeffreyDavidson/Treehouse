a42a0263d3efca11422196675ad173620097016d
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-08-27 16:43:36 -0400

    Learn about the negation operator.

--- PHP/PHP Arrays and Control Structures/conditionals.php
+++ PHP/PHP Arrays and Control Structures/conditionals.php
@@ -3,6 +3,49 @@
 $a = 0;
 $b = 5;
 
+/*
+if ($a == $b) {
+    echo 'values are equal';
+} else {
+    echo 'values are NOT equal';
+}
+
+if (($a == $b) == false) {
+    echo 'values are  NOT equal';
+}
+
+if ($a <> $b) {
+    echo 'values are  NOT equal';
+}
+*/
+
+/*
+if ($a != $b) {
+    echo 'values are  NOT equal';
+}
+
+if ($a !== $b) {
+    echo 'NOT identical';
+}
+*/
+
+if ($a) {
+    echo 'true';
+}
+
+if (!$a) {
+    echo 'false';
+}
+
+if ($a == false) {
+    echo 'false';
+}
+
+if ($a != true) {
+    echo 'false';
+}
+
+/*
 if ($a == $b) {
     echo ' values are equal ';
 }  elseif ($a < $b) {
@@ -12,7 +55,9 @@ if ($a == $b) {
 } else {
     echo ' values are NOT equal ';
 }
+*/
 
+/*
 $score = 30;
 if ($score >= 59) {
     echo 'You completed the level!';
@@ -21,5 +66,5 @@ if ($score >= 59) {
 } else {
     echo 'Please try again';
 }
-
+*/
 ?>

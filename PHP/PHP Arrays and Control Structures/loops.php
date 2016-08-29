af5fda73b4d83bef0bcc51e5b1ad8c7303d9731e
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-08-29 10:44:25 -0400

    Learn how to use for loops in logic.

--- PHP/PHP Arrays and Control Structures/loops.php
+++ PHP/PHP Arrays and Control Structures/loops.php
@@ -1,7 +1,10 @@
 <?php
 
-$currentYear = date('Y');
-$year = $currentYear - 100;
+/*
+for ($year = date('Y') - 99; $year <= date('Y'); $year++) {
+    echo $year . "<br />\n";
+}
+*/
 
 /*while (++$year <= $currentYear) {
     echo $year . "<br />\n";
@@ -17,11 +20,16 @@ $learn = array('Conditionals', 'Arrays', 'Loops');
 $learn[] = 'Build something awesome!';
 array_push($learn, 'Functions', 'Forms', 'Objects');
 array_unshift($learn, 'HTML', 'CSS');
-asort($learn);
+sort($learn);
+
+for ($i = 0; $i < count($learn); $i++) {
+    echo $learn[$i] . "\n";
+}
 
 $count = 0;
-while ((list($key, $val) = each($learn)) && $count++ < 2) {
+/*while ((list($key, $val) = each($learn)) && $count++ < 2) {
     echo "$key => $val\n";
 }
+*/
 
 ?>

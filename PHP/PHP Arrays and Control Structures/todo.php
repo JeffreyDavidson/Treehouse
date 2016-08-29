d25300a440dda91deffaaf5ffa1d46a46e831cf3
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-08-29 11:39:24 -0400

    Learn how to use a foreach loop on an associative array.

--- PHP/PHP Arrays and Control Structures/todo.php
+++ PHP/PHP Arrays and Control Structures/todo.php
@@ -1,20 +1,35 @@
 <?php
 
-$list[] = [
-    'title' => 'Laundry',
-    'priority' => 2,
-    'due' => '',
-    'complete' => true,
-];
+include 'list.php';
 
-$list[] = [
-    'title' => 'Clearn out refridgerator',
-    'priority' => 3,
-    'due' => '07-30-2016',
-    'complete' => false,
-];
+foreach ($list as $key => $item) {
+    echo $key . ' = ' . $item['title'] .  "<br />\n";
+}
 
-var_dump($list);
+echo '<table>';
+echo '<tr>';
+echo '<th>Title</th>';
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Completed</th>';
+echo '</tr>';
+foreach ($list as $item) {
+    echo '<tr>';
+    echo '<td>' . $item['title'] . "</td>\n";
+    echo '<td>' . $item['priority'] . "</td>\n";
+    echo '<td>' . $item['due'] . "</td>n";
+    echo '<td>';
+    if ($item['complete']) {
+        echo 'Yes';
+    } else {
+        echo 'No';
+    }
+    echo "</td>\n";
+    echo '</tr>';
+}
+echo '</table>';
+
+//var_dump($list);
 //echo $list[0]['title'];
 
 ?>

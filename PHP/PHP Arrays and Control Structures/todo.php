fe98774d0c336524f31b447d5ce1bfdaac197f0c
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2016-08-28 21:06:05 -0400

    Learn the benefits of multidimensional arrays and how to access inner array keys and values.

---
+++ PHP/PHP Arrays and Control Structures/todo.php
@@ -0,0 +1,20 @@
+<?php
+
+$list[] = [
+    'title' => 'Laundry',
+    'priority' => 2,
+    'due' => '',
+    'complete' => true,
+];
+
+$list[] = [
+    'title' => 'Clearn out refridgerator',
+    'priority' => 3,
+    'due' => '07-30-2016',
+    'complete' => false,
+];
+
+var_dump($list);
+//echo $list[0]['title'];
+
+?>

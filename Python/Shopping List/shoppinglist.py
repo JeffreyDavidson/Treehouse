011654c0501b33265bf55e34cc8edd7eff0d1bc9
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2018-05-31 16:17:22 -0400

    Update functionality.

--- Python/Shopping List/shoppinglist.py
+++ Python/Shopping List/shoppinglist.py
@@ -1,11 +1,17 @@
+import os
+
 shopping_list = []
 
 def add_to_list(item):
     shopping_list.append(item)
     print("Added! List has {} items.".format(len(shopping_list)))
+    show_list()
 
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def show_help():
+    clear_screen()
     print("What should we pick up at the store?")
     print("""
     Enter 'DONE' to stop adding items.
@@ -14,21 +20,22 @@ def show_help():
     """)
 
 def show_list():
+    clear_screen()
     print("Here's your list:")
     for item in shopping_list:
         print(item)
 
 while True:
     new_item = input("> ")
-    if new_item == "DONE":
+    if new_item.upper == "DONE" or new_item.upper == "QUIT":
         break
-    elif new_item == 'HELP':
+    elif new_item.upper == 'HELP':
         show_help()
         continue
-    elif new_item == "SHOW":
+    elif new_item.upper == "SHOW":
         show_list()
         continue
-
-    add_to_list(new_item)
+    else:
+        add_to_list(new_item)
 
 show_list()
